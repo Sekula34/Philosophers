2febc37ec6832edea9af9198ce603fb04513360c
Filip Seles <fseles@c4r7p5.42vienna.com>
2024-01-26 15:28:38 +0100

    philos set up

---
+++ philo/philosophers.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fseles <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 15:23:02 by fseles            #+#    #+#             */
+/*   Updated: 2024/01/26 15:23:04 by fseles           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+# define OK 0
+# define FAIL 1
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+
+#endif
