1b6210e16eaffd0106aba17c44c522262665fe8a
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-01-29 12:33:49 +0100

    prepare

--- philo/main.c
+++ philo/main.c
@@ -11,12 +11,15 @@
 /* ************************************************************************** */
 
 #include "philosophers.h"
+	pthread_mutex_t kljuc;
 
 void *my_thread(void *argp)
 {
 	int value; 
-	value = *(int *)argp; 
+	value = *(int *)argp;
+	pthread_mutex_lock(&kljuc);
 	printf("cao ja sam dretva, %d \n", value);
+	pthread_mutex_unlock(&kljuc);
 	return NULL;
 }
 
@@ -27,6 +30,9 @@ int main()
 	int value_maiin = 2;
 
 
+
+	if(pthread_mutex_init(&kljuc, NULL) != 0)
+		return(perror("pao init"), FAIL);
 	void *v_p;
 
 	v_p = &value_maiin;
@@ -35,11 +41,16 @@ int main()
 		perror("gettimeofday failed");
 		return (FAIL);
 	}
+	pthread_mutex_lock(&kljuc);
 	printf("trenutno vrijeme je %ld sekundi i %ld mikrosekundi\n", my_time.tv_sec, my_time.tv_usec);
+	pthread_mutex_unlock(&kljuc);
 	if(pthread_create(&first_thread, NULL, &my_thread, v_p) != 0)
 		return (FAIL);
 	//if(pthread_detach(first_thread) != 0)
 		//return (perror("fail"), FAIL);
+	if(pthread_join(first_thread, NULL) != 0)
+		return(perror("spoj je faileo"), FAIL);
+	printf("spojeni smo\n");
 	//printf("Dretva otkacena\n");
 	usleep(5 * MICRO);
 	if(gettimeofday(&my_time, NULL) != 0)
@@ -49,8 +60,6 @@ int main()
 	}
 	printf("trenutno vrijeme je %ld sekundi i %ld mikrosekundi\n", my_time.tv_sec, my_time.tv_usec);
 	printf("hello i am filip\n");
-	if(pthread_join(first_thread, NULL) != 0)
-		return(perror("spoj je faileo"), FAIL);
-	printf("spojeni smo\n");
+	pthread_mutex_destroy(&kljuc);
 	return(OK);
 }
