5d1b4e8b27d8f0f20d8b1fdf39b136f3a36e34c9
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-01-29 12:04:18 +0100

    thread prepare

--- philo/main.c
+++ philo/main.c
@@ -12,23 +12,35 @@
 
 #include "philosophers.h"
 
-// void *my_thread(void *argp)
-// {
-// 	printf("cao ja sam dretva \n");
-// 	return NULL;
-// }
+void *my_thread(void *argp)
+{
+	int value; 
+	value = *(int *)argp; 
+	printf("cao ja sam dretva, %d \n", value);
+	return NULL;
+}
 
 int main()
 {
 	struct timeval my_time;
+	pthread_t first_thread;
+	int value_maiin = 2;
 
+
+	void *v_p;
+
+	v_p = &value_maiin;
 	if(gettimeofday(&my_time, NULL) != 0)
 	{
 		perror("gettimeofday failed");
 		return (FAIL);
 	}
 	printf("trenutno vrijeme je %ld sekundi i %ld mikrosekundi\n", my_time.tv_sec, my_time.tv_usec);
-	//if(pthread_create())
+	if(pthread_create(&first_thread, NULL, &my_thread, v_p) != 0)
+		return (FAIL);
+	//if(pthread_detach(first_thread) != 0)
+		//return (perror("fail"), FAIL);
+	//printf("Dretva otkacena\n");
 	usleep(5 * MICRO);
 	if(gettimeofday(&my_time, NULL) != 0)
 	{
@@ -37,5 +49,8 @@ int main()
 	}
 	printf("trenutno vrijeme je %ld sekundi i %ld mikrosekundi\n", my_time.tv_sec, my_time.tv_usec);
 	printf("hello i am filip\n");
+	if(pthread_join(first_thread, NULL) != 0)
+		return(perror("spoj je faileo"), FAIL);
+	printf("spojeni smo\n");
 	return(OK);
 }
