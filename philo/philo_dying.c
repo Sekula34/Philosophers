934e7b8e16af9aee099683663e2cbb59f518f3b8
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-01-31 18:29:02 +0100

    philo norm

--- philo/philo_dying.c
+++ philo/philo_dying.c
@@ -14,30 +14,29 @@
 
 //check if eater is dad 
 //if it is return 1 and unlock number of taken mutexes from number of mutexes
-int am_i_dead(t_eater *diogen, int number_of_mutex)
+int	am_i_dead(t_eater *diogen, int number_of_mutex)
 {
-	int status;
+	int	status;
 
-	
 	status = 0;
 	pthread_mutex_lock(&diogen->philo->meal_mut);
-	if(diogen->person->dead_flag == 1)
+	if (diogen->person->dead_flag == 1)
 		status = 1;
 	pthread_mutex_unlock(&diogen->philo->meal_mut);
-	if(status == 0)
+	if (status == 0)
 		return (0);
-	if(number_of_mutex == 0)
-		return(1);
-	if(number_of_mutex == 1)
+	if (number_of_mutex == 0)
+		return (1);
+	if (number_of_mutex == 1)
 	{
 		pthread_mutex_unlock(diogen->first_fork);
-		return(1);
+		return (1);
 	}
-	if(number_of_mutex == 2)
+	if (number_of_mutex == 2)
 	{
 		pthread_mutex_unlock(diogen->first_fork);
 		pthread_mutex_unlock(diogen->second_fork);
-		return(1);
+		return (1);
 	}
-	return(0);
+	return (0);
 }
